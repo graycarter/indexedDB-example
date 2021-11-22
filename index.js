cbe1552599fc452e7e85eee383c6c2a4d282bd27
Grayson Carter <graysoncarter1999@gmail.com>
2021-11-22 12:36:23 +0300

    Stored data to IndexedDB with localforage

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
+const uuid = require("uuid");
+const localforage = require("localforage");
 let local = document.getElementById("local");
 let btn = document.getElementById("btn");
 let user = {
   firstName: "",
   lastName: "",
+  email: ""
 };
+var user_store = localforage.createInstance({
+  name: "users",
+});
 
 btn.onclick = () => {
   local.onsubmit = (event) => {
     var data = new FormData(local).values();
-    user.firstName = data.next().value
-    user.lastName = data.next().value
-    localforage
-      .setItem("user", user)
-      .then(function (value) {
-        console.log(value);
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
+    user.firstName = data.next().value;
+    user.lastName = data.next().value;
+    user_store.setItem(uuid.v4(), user);
     event.preventDefault();
   };
 };
